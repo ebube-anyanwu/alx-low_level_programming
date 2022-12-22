dc25599c201224d4d89427e89dbe2be7c562a926
ebube-anyanwu <ebubechianyanwu1@google.com>
2022-12-22 15:37:12 -0800

    last changes

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - a function that returns the length of a string
+ * @s: a pointer
+ * Return: int
+ */
+
+int _strlen(char *s)
+{
+	int i, len;
+	
+	for(i = 0; s[i] != '\0'; i++)
+		;
+	return(i);
+}
