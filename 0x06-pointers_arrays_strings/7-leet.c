c1c469dabd52697f3ea2ecc368a0b5ff2b932b89
ebube-anyanwu <ebubechianyanwu1@google.com>
2023-01-05 09:33:55 -0800

    last changes

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -15,7 +15,7 @@ char *leet(char *s)
 	{
 		for (j = 0; j < 8; j++)
 		{
-			if (s[i] == leet[j] || (s[i] -32) == leet[j])
+			if (s[i] == leet[j] || (s[i] - 32) == leet[j])
 				s[i] = j + '0';
 		}
 		i++;
