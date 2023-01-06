232398ed0a27a7776dbc24a184515fd5c72f60b1
ebube-anyanwu <ebubechianyanwu1@google.com>
2023-01-06 06:32:44 -0800

    last changes

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,17 @@
+/**
+ * _strchr - function that locates a character in a string.
+ * @s: string
+ * @c: character
+ * Return: pointer to s if character is found else NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return (0);
+}
