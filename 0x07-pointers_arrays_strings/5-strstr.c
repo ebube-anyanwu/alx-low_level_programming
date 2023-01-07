8888d98d6718677dfe3d2f4e9902af781cc32b36
ebube-anyanwu <ebubechianyanwu1@google.com>
2023-01-06 18:13:22 -0800

    last changes

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,23 @@
+/**
+ * _strstr - function that locates a substring.
+ * @haystack: string
+ * @needle: whatever
+ * Return: a pointer to the byte in s that matches one of the bytes in accept,
+ * or NULL if no such byte is found
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int index;
+
+	while (*needle != '\0')
+	{
+		for (index = 0; haystack[index] != '\0'; index++)
+		{
+			if (*needle == haystack[index])
+				return (haystack + index);
+		}
+		needle++;
+	}
+	return (0);
+}
