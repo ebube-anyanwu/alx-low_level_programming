2911d183672c3f241d9874eefed88700def32b4d
ebube-anyanwu <ebubechianyanwu1@google.com>
2023-01-10 03:52:23 -0800

    last changes

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,21 +1,27 @@
 #include "main.h"
 
 /**
- *  _sqrt_recursion- a function  that prints returns the natural square
- *  root of a number.
+ * find_root - a function that recursively finds the square root of n
  * @n: the number
- * Return: -1 if y < 0, 1 if n == 0 else return power.
+ * @guess: the root
+ * Return: the root
  */
 
 int find_root(int n, int guess)
 {
 	if (n == (guess * guess))
 		return (guess);
-	if (guess == n/2)
+	if (guess == n / 2)
 		return (-1);
-
 	return (find_root(n, guess + 1));
 }
+
+/**
+ *  _sqrt_recursion - a function  that prints returns the natural square
+ *  root of a number.
+ * @n: the number
+ * Return: -1 if y < 0, 1 if n == 0 else return power.
+ */
 
 int _sqrt_recursion(int n)
 {
