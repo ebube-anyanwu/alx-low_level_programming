10c9d5b877ee72623e6105f9356dc2cd8866c888
ebube-anyanwu <ebubechianyanwu1@google.com>
2023-01-12 07:35:35 -0800

    last changes

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - a program that prints its name, followed by a new line.
+ * @argc: argument count
+ * @argv: argument value
+ * Return: 0 on success.
+ */
+
+int main(int argc, char **argv)
+{
+	while (*argv)
+	{
+		printf("%s\n", *argv);
+		argv++;
+	}
+	return (0);
+}
