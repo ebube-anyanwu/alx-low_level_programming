cdd042b2ea8d825188f13e968d87d92a0ee36faf
ebube-anyanwu <ebubechianyanwu1@google.com>
2023-01-12 07:20:32 -0800

    last changes

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - a program that prints its name, followed by a new line.
+ * @argc: argument count
+ * @argv: argument value
+ * Return: 0 on success.
+ */
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
