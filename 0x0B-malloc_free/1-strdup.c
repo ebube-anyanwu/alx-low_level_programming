b111cdab9cc0b468d57d61ef849df4871042143d
ebube-anyanwu <ebubechianyanwu1@google.com>
2023-01-16 07:45:07 -0800

    last changes

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -19,7 +19,7 @@ char *_strdup(char *str)
 	for (len = 0; str[len] != '\0'; len++)
 		;
 
-	s = malloc(len + 1 * sizeof(char));
+	s = malloc((len + 1) * sizeof(char));
 
 	if (s == NULL)
 		return (NULL);
