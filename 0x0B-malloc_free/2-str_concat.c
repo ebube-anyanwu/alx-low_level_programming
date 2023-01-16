436b15c1031fd06097d87579bed04d46ce375508
ebube-anyanwu <ebubechianyanwu1@google.com>
2023-01-16 08:18:26 -0800

    last changes

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,8 +14,12 @@ char *str_concat(char *s1, char *s2)
 	char *s;
 	int i, len, index = 0;
 
-	if (s1 == NULL && s2 == NULL)
-		return (NULL);
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
 	for (i = 0; s1[i] != '\0'; i++)
 		len++;
 	for (i = 0; s2[i] != '\0'; i++)
