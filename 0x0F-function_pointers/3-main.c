2e6151da0c32a5e1f08592858cf0736aa5472379
ebube-anyanwu <ebubechianyanwu1@google.com>
2023-02-02 03:13:20 -0800

    last changes

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,43 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: The number of command line arguments
+ * @argv: command line arguments.
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+	char *operator;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	operator = argv[2];
+
+	if (get_op_func(operator) == NULL || operator[1] != '\0')
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if ((*operator == '/' && num2 == 0) || (*operator == '%'
+		&& num2 == 0))
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	printf("%d\n", get_op_func(operator)(num1, num2));
+
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,64 @@
+#include "3-calc.h"
+
+/**
+ * op_add - function that returns the sum of two integers
+ * @a: param1
+ * @b: param2
+ * Return: the sum of a and b
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - function that returns the difference between two
+ * integers
+ * @a: param1
+ * @b: param2
+ * Return: the difference between a and b
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - function that returns the product of two integers
+ * @a: param1
+ * @b: param2
+ * Return: the product of a and b
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - function that returns the result of the
+ * division of two integers
+ * @a: param1
+ * @b: param2
+ * Return: the result of the division of a and b
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - function that returns th remainder of the division
+ * of two integers
+ * @a: param1
+ * @b: param2
+ * Return: the remainder of the division of a and b
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
