87f50db0a1dfe987a1a7dc8a64328f53c8c968c4
ebube-anyanwu <ebubechianyanwu1@google.com>
2023-02-06 10:34:52 -0800

    last changes

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,94 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_char: function to print a char
+ * @args: argument of type va_list
+ */
+
+void print_char(va_list args)
+{
+	char c = va_arg(args, int);
+
+	printf("%c", c);
+}
+
+/**
+ * print_integer: function to print an integer.
+ * @args: argument of type va_list
+ */
+
+void print_integer(va_list args)
+{
+	int i = va_arg(args, int);
+
+	printf("%d", i);
+}
+
+/**
+ * print_float: function to print a float.
+ * @args: argument of type va_list
+ */
+
+void print_float(va_list args)
+{
+	float f = va_arg(args, double);
+
+	printf("%f", f);
+}
+
+/**
+ * print_string: function to print a string.
+ * @args: argument of type va_list
+ */
+
+void print_string(va_list args)
+{
+	char *s = va_arg(args, char *);
+
+	if (s == NULL)
+		printf("(nil)");
+	printf("%s", s);
+}
+
+
+/**
+ * print_all - a function that prints anything.
+ * @format: a list of types of arguments passed to the function
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	char *separator = "";
+	int i = 0, j = 0;
+	print_t f[] = {
+		{"c", print_char},
+		{"i", print_integer},
+		{"f", print_float},
+		{"s", print_string}
+	};
+
+	va_start(args, format);
+
+	while (format[i] != '\0')
+	{
+		j = 0;
+
+		while(j < 4 && (format[i] != *(f[j].symbol)))
+			j++;
+
+		if (j < 4)
+		{
+			printf("%s", separator);
+			f[j].printit(args);
+			separator = ", ";
+		}
+
+		i++;
+	}
+	printf("\n");
+
+	va_end(args);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,19 @@
 #ifndef VARIADIC_FUNCTION_H
 #define VARIADIC_FUNCTION_H
+#include <stdarg.h>
+
+/**
+ * struct print - structure to point to a
+ * function depending matching symbol
+ * just go through the code, you'll understand.
+ * @symbol: that's the symbol I was just telling you about.
+ * @printit: pointer to function. Stay with me.
+ */
+
+typedef struct print{
+	char *symbol;
+	void(*printit)(va_list);
+} print_t;
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
